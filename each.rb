6954db747227a11caf95fca6605a41583ad4ef29
Daniel Sauer <danieljsauer92@gmail.com>
2023-03-29 17:49:49 -0600

    Solves challenge 2

--- each.rb
+++ each.rb
@@ -11,3 +11,9 @@ weather_options = ["sunny", "snowy", "freezing", "partly cloudy", "drizzly"]
 words = ["car", "bike", "bus", "van", "scooter", "truck", "rv"]
 
 # Record yourself live-coding and talking through either Challenge 1 or 2. The recording doesn't have to be the first time you are solving the problem. Use technical vocabulary and explain each part to demonstrate your understanding! Share the link to your video in your small group channel.
+
+words.each do |word|
+  if word.length == 3
+    puts word 
+  end
+end 
